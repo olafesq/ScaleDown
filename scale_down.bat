096e02ff5d1e44927e43f2178eb674409a77fad0
olafesq <olafesq@gmail.com>
2015-12-10 23:39:24 +0200

    Update scale_down.bat

--- scale_down.bat
+++ scale_down.bat
@@ -2,6 +2,7 @@
 
 :: This ensures that command convert is defined with full path and not confused with other convert.exe or simlilar stuff
   SETLOCAL EnableDelayedExpansion 
+  ::Modify the path as needed:
   SET IMCONV="C:\Programs\ImageMagick-6.8.9-Q16\convert.exe"
   
   :: geometry option flag '^' which is used to resize the image based on the smallest fitting dimension. That is, the image is resized to completely fill (and even overflow) the pixel area given. 
